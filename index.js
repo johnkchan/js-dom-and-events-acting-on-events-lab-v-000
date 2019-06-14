4e138e03d0b826572714c1812833587061de2686
Jonathan Chan <johnkchan252@gmail.com>
2019-06-14 00:22:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,6 +23,10 @@ function addNewLiOnClick() {
 
 function clearEmployeeListOnLinkClick() {
   document.body.querySelector('a').addEventListener('click', function(event) {
-    document.querySelector('employee-list').
+    let list = document.querySelector('employee-list')
+    
+    while (list.firstChild) {
+        list.removeChild(list.firstChild);
+    }
   });
 }
