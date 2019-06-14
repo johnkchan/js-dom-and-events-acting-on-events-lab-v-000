8e8fd1ad583a85081605704ce14c3ff2dae00922
Jonathan Chan <johnkchan252@gmail.com>
2019-06-14 00:16:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function retrieveEmployeeInformation() {
 }
 
 function addNewElementAsLi() {
-  document.querySelector('.employee-list');
+  document.querySelector('.employee-list').appendChild;
 }
 
 function addNewLiOnClick() {
